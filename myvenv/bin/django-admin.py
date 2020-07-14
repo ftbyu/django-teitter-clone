ebba0f421425679eca60f70663eea538f71173e4
ftbyu <ftb.y.u.kashima.07@outlook.jp>
2020-07-14 17:16:28 +0900

    first commit

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/murakami/Documents/work/python/django-twitter-clone/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
